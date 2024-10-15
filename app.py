c3f0e04b5be1b909d9e85f1db120efe908c7f6c2
Dashazem <dashazemska@gmail.com>
2024-10-15 21:11:37 +0200

    updated requirementes

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ CORS(app, supports_credentials=True)
 
 load_dotenv()
 
+mysql = MySQL(app)
+
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-
-mysql = MySQL(app)
 
 
 @app.route('/<path:path>')
