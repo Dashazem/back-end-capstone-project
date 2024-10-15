4d10e9224ae88f6ca5c182b8e7d07f810670fbda
Dashazem <dashazemska@gmail.com>
2024-10-15 22:08:42 +0200

    changed mysql

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import requests
 import bcrypt
 from flask import Flask, jsonify, request, send_from_directory
-import mysql.connector
+from flask_mysqldb import MySQL
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -13,13 +13,12 @@ CORS(app, supports_credentials=True)
 
 load_dotenv()
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host=os.getenv('MYSQL_HOST'),
-        user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
-        database=os.getenv('MYSQL_DB')
-    )
+mysql = MySQL(app)
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 
 @app.route('/<path:path>')
